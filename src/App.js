3abcc743fcf440eebf9c611b062229e7e8d1e75d
Diego <diego2003coila@gmail.com>
2023-10-20 09:14:11 -0500

    v06 clase 20/10/2023, creacion de cuenta  de firebase, configuracion del firebase en google, creación de firebase con su js, modificacion de firabes.js (auth, storage, firestore)

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Appform from './components/Appform';
 //import C01componente from './components/C01componente';
 //import Variable from './components/C04variable';
+//import MatrizOperaciones from './components/P7matrizOperaciones';
 
 
 function App() {
@@ -11,6 +12,10 @@ function App() {
       <h1>App.js</h1>
       <Appform></Appform>
       <i class="large material-icons">insert_chart</i>
+
+      <p>1. Juan Manuel 23 Masculino-------------x-----A</p>
+      <p>2. Rosa Maria  25 Femenino---------------x-----A</p>
+      <p>3. Luis Miguel 40 Masculino--------------x-----A</p>
     </div>
   );
 }
