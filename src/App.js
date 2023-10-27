c15675ee785d2206b73409d7a08408b8c9c4a7ed
Diego <diego2003coila@gmail.com>
2023-10-27 08:26:28 -0500

    V7. ya resgristra previa validacion y usa el idActual para guarda o actualizar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 //import logo from './logo.svg';
 import './App.css';
 import Appform from './components/Appform';
+import { useState } from 'react';
 //import C01componente from './components/C01componente';
 //import Variable from './components/C04variable';
 //import MatrizOperaciones from './components/P7matrizOperaciones';
@@ -8,10 +9,21 @@ import { db } from "./firebase/firebase";
 
 
 function App() {
+
+   ///// READ - LECTURA - fnRead //////
+   const [docBD, setdocBD] = useState([]);
+   const fnRead = () => {
+   }
+   ///// DELETE - ELIMINAR - fnDelete /////
+   const [idActual, setIdActual] = useState("");
+   const fnDelete =(xId) =>{
+ 
+   }
+
   return (
     <div style={{background:"yellow", width:"350px", padding:"10px", textAlign:"center"}}>
       <h1>App.js</h1>
-      <Appform></Appform>
+      <Appform {...{idActual}}></Appform>
       <i class="large material-icons">insert_chart</i>
 
       <p>1. Juan Manuel 23 Masculino-------------x-----A</p>
